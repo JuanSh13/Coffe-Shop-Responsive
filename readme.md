25df23613a39c2151b50a6c5ff4175ffbf136725
shjuanmsh <shjuanmsh@gmail.com>
2023-06-30 20:44:46 -0600

    Modification of the files and the structure of the page

---
+++ readme.md
@@ -0,0 +1,3 @@
+# REFERENCE - Year of Creation(2022)
+
+- https://www.youtube.com/watch?v=oP9HBE-4Wxw
---
+++ script/main.js
@@ -0,0 +1,23 @@
+const navbar = document.querySelector('.navbar');
+const search = document.querySelector('.search-box');
+
+let mediaqueryList = window.matchMedia("(min-width: 769px)");
+
+document.querySelector('.navbar-responsive').onclick = () => {
+  navbar.classList.remove('inactiveNav');
+  navbar.classList.add('activeNav');
+
+};
+document.querySelector('.navbar-closed').onclick = () => {
+  navbar.classList.add('inactiveNav');
+  navbar.classList.remove('activeNav');
+};
+document.querySelector('#search-icon').onclick = () => {
+  search.classList.toggle('active');
+};
+
+if(mediaqueryList.matches) {
+  navbar.classList.remove('inactiveNav');
+  navbar.classList.remove('activeNav');
+}
+
