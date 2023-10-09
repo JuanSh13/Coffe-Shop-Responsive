a0d589d9d5fdeaab0fdfbfb7acc54819c7631e7c
shjuanmsh <shjuanmsh@gmail.com>
2023-10-09 12:16:18 -0600

    Add Swipper Slides

---
+++ scripts/swipper.js
@@ -0,0 +1,32 @@
+// swiper element
+const swiperEl = document.querySelector('swiper-container');
+
+// swiper parameters
+const swiperParams = {
+  slidesPerView: 1,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+    769: {
+      slidesPerView: 2,
+    },
+    1025: {
+      slidesPerView: 3,
+    },
+  },
+  on: {
+    init() {
+      // ...
+    },
+  },
+};
+
+// now we need to assign all parameters to Swiper element
+Object.assign(swiperEl, swiperParams);
+
+// and now initialize it
+swiperEl.initialize();
